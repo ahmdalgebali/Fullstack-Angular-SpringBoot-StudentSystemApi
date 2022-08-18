2c2262b8d760773c02c4c5d7c9b8042cfde16105
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-08-18 16:27:03 +0200

    Add Lombok to Student.java closes #7

--- back-End/src/main/java/com/spring/student/studentsystem/model/Student.java
+++ back-End/src/main/java/com/spring/student/studentsystem/model/Student.java
@@ -11,7 +11,15 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "student")
 public class Student {
     @Id
