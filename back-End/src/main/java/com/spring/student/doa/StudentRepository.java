70bc7c62ff6b54c961623dbebbad0400125a69af
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-08-19 01:18:32 +0200

    add api configration closes #9

--- back-End/src/main/java/com/spring/student/doa/StudentRepository.java
+++ back-End/src/main/java/com/spring/student/doa/StudentRepository.java
@@ -2,13 +2,11 @@ package com.spring.student.doa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 import com.spring.student.model.Student;
 
 @RepositoryRestResource
 public interface StudentRepository extends JpaRepository <Student,Long> {
 
 
-
-    
+  
 }
