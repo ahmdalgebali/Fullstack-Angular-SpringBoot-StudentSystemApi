e0f5bb71132f825845a4749188232bb347527ae5
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-08-20 15:23:19 +0200

    Add register component closes #13

--- front-End/src/app/app.module.ts
+++ front-End/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FotterComponent } from './components/fotter/fotter.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FotterComponent
+    FotterComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
