4bd20b183833c6f81f09c97565fa889be9170a50
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-08-20 19:35:07 +0200

    create model Student closes #16

---
+++ front-End/src/app/model/student.ts
@@ -0,0 +1,21 @@
+export class Student {
+    id: number;
+    fullName: string;
+    gender: string;
+    age: string;
+    phone: string;
+    address: string;
+    dateCreated: Date;
+    dateUpdated: Date;
+    constructor(id: number, fullName: string, gender: string, age: string, phone: string, address: string , dateCreated: Date , dateUpdated: Date) {
+      this.id = id;
+      this.fullName = fullName;
+      this.gender = gender;
+      this.age = age;
+      this.phone = phone;
+      this.address = address;
+      this.dateCreated = dateCreated;
+      this.dateUpdated = dateUpdated;
+    }
+
+  }
