ad8f0abbc930733ecff1e6b2db670eb673130a81
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-08-20 15:00:45 +0200

    create header and fotter components closes #12

--- front-End/src/app/app.module.ts
+++ front-End/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FotterComponent } from './components/fotter/fotter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FotterComponent
   ],
   imports: [
     BrowserModule,
