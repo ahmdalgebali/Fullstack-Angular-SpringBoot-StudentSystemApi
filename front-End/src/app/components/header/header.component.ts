dd0cdfac2d21def4bea18938fe3b17658b4b4f4f
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-08-22 16:30:34 +0200

    Register Service & Handel components closes #21

--- front-End/src/app/components/header/header.component.ts
+++ front-End/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService:LoginService) { }
 
   ngOnInit(): void {
+    // TODO document why this method 'ngOnInit' is empty
+  
+  }
+
+  isAuthenticaterUser(){
+    // return this.isAuthenticaterUser();
   }
 
 }
