b5b434127e12a0372f7d8ac724ab4855c109087b
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-08-24 20:21:35 +0200

    Spring get all students get student closes #28

--- back-End/src/main/java/com/spring/student/controller/StudentController.java
+++ back-End/src/main/java/com/spring/student/controller/StudentController.java
@@ -1,12 +1,20 @@
 package com.spring.student.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.student.model.Student;
 import com.spring.student.service.StudentService;
 
 @CrossOrigin("http://localhost:4200/")
 @RestController
+@RequestMapping("system/")
 public class StudentController {
     private StudentService studentService;
 
@@ -14,5 +22,20 @@ public class StudentController {
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
+
+    @GetMapping("students")
+    public List<Student> getStudent(){
+        return studentService.getStudent();
+    }
+
+    @GetMapping("student")
+    public Student getStudentById(@RequestParam Long id){
+        return studentService.getStudentById(id);
+
+
+    }
+
+
+
 
 }
--- back-End/src/main/java/com/spring/student/service/StudentService.java
+++ back-End/src/main/java/com/spring/student/service/StudentService.java
@@ -1,8 +1,11 @@
 package com.spring.student.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.spring.student.doa.StudentRepository;
+import com.spring.student.model.Student;
 
 @Service
 public class StudentService {
@@ -11,6 +14,15 @@ public class StudentService {
     @Autowired
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudent(){
+        return studentRepository.findAll();
+    }
+    public Student getStudentById(Long id){
+        return studentRepository.findById(id).get();
+
+
     }
 
 }
