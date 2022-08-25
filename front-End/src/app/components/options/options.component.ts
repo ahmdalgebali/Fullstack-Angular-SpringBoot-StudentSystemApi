196e4269fa9ce5f2d4ec213956969c8359167f21
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-08-25 19:02:29 +0200

    implement formbuilder to update and add methode closes #33

--- front-End/src/app/components/options/options.component.ts
+++ front-End/src/app/components/options/options.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-options',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./options.component.css']
 })
 export class OptionsComponent implements OnInit {
+  StudentGroup: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.StudentGroup = this.formBuilder.group({
+      student: this.formBuilder.group({
+        fullName: [''],
+        Age: [''],
+        Address: [''],
+        phoneNumber: [''],
+        gender: ['MALE']
+      })
+
+
+    });
   }
 
+
+  get fullName(){
+    return this.StudentGroup.get('student.fullName')
+  }
+  get Age(){
+    return this.StudentGroup.get('student.Age')
+  }
+  get Address(){
+    return this.StudentGroup.get('student.Address')
+  }
+  get phoneNumber(){
+    return this.StudentGroup.get('student.phoneNumber')
+  }
+  get gender(){
+    return this.StudentGroup.get('student.gender')
+  }
+  
+  Done() {
+
+      console.log(this.fullName?.value);
+      console.log(this.Age?.value);
+      console.log(this.Address?.value);
+      console.log(this.phoneNumber?.value);
+      console.log(this.gender?.value)
+  }
 }
