f194ec0798a0354ddc692f7dad101954c9605ed0
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-08-26 21:02:52 +0200

    validation login component closes #38

--- front-End/src/app/components/register/register.component.ts
+++ front-End/src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup ,FormControl , Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/services/login.service';
 
@@ -11,45 +11,51 @@ import { LoginService } from 'src/app/services/login.service';
 export class RegisterComponent implements OnInit {
 
   logInFormGroup: FormGroup;
-  invalidMessage:string;
+  invalidMessage: string;
   constructor(private formBuilder: FormBuilder,
-              private loginService: LoginService,
-              private route: Router) { }
+    private loginService: LoginService,
+    private route: Router) { }
 
   ngOnInit(): void {
     this.logInFormGroup = this.formBuilder.group({
       admin: this.formBuilder.group({
-        userName: [''],
-        password: ['']
+        userName: new FormControl('', [Validators.required, Validators.minLength(5)]),
+        password: new FormControl('', [Validators.required, Validators.minLength(5)])
       })
     });
   }
 
-  get userName(){
+  get userName() {
     return this.logInFormGroup.get('admin.userName')
   }
-  get password(){
+  get password() {
     return this.logInFormGroup.get('admin.password')
   }
 
-  
+
   OnSubmit() {
-   const result= this.loginService.Login(this.userName?.value,this.password?.value)
-   if(result==true){
-    this.route.navigateByUrl('students');
-   }else{
-    this.invalidMessage="Invalid Username or Password";
-    this.showMessage()
-   }
-    
-      // console.log(this.userName?.value)
-      // console.log(this.password?.value)
+    if (this.logInFormGroup.invalid) {
+      this.logInFormGroup.markAllAsTouched();
+    } else {
+      const result = this.loginService.Login(this.userName?.value, this.password?.value)
+      if (result == true) {
+        this.route.navigateByUrl('students');
+      } else {
+        this.invalidMessage = "Invalid Username or Password";
+        this.showMessage()
+      }
+
+    }
+
+
+    // console.log(this.userName?.value)
+    // console.log(this.password?.value)
   }
 
-  showMessage(){
+  showMessage() {
     setTimeout(() => {
       this.invalidMessage = ""
-    },3000)
+    }, 3000)
   }
 
 
